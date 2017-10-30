d3251201526e1b4999686e98aa001f187fe9ac0a
zja <zja0809@foxmail.com>
2017-10-30 13:44:14 +0800

    修改 join_example.py

--- join_example.py
+++ join_example.py
@@ -23,7 +23,7 @@ def run_c2():
 
 def run_c3():
     time.sleep(5)
-    for i in range(50):
+    for i in range(100):
         print("c3-%s\n" % i)
 
 c1 = threading.Thread(name="c1",target=run_c1)
@@ -34,9 +34,10 @@ c3.setDaemon(True)  # 设为守护进程，随主进程终止而终止
 print 'start ...\n'
 c1.start()
 c2.start()
-# c3.start()
+c3.start()
 
 c1.join(3)  # 5秒之后，join超时，此时c1仍继续执行，但主线程不再等待c1
 c2.join()   # 无设置超时时间，主线程挂起，等待c2,c3执行完毕
 print "isAlive:"+str(c1.isAlive())+'\n'    # c1未执行完毕，则isAlive为True
 print 'the end ...\n'   # 需等待c2执行完毕；需等待至c1的join超时或者c1执行完毕
+# c1、c2终止后，主线程才能终止
